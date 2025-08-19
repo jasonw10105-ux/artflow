f3fa6fda83ecb9e4736a9870d18033f7bd603a30
jasonw10105-ux <jasonw10105@gmail.com>
2025-08-19 14:05:28 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
-import ArtworkManagement from './pages/ArtworkManagement'
+import ArtworkList from './pages/ArtworkList'
+import ArtworkCreate from './pages/ArtworkCreate'
+import ArtworkEdit from './pages/ArtworkEdit'
 import CatalogueManagement from './pages/CatalogueManagement'
 import PublicCatalogue from './pages/PublicCatalogue'
 import PrivateCatalogue from './pages/PrivateCatalogue'
@@ -32,9 +34,10 @@ function App() {
             <Route path="set-password" element={<PublicRoute><SetPassword /></PublicRoute>} />
             <Route path="auth/callback" element={<PublicRoute><AuthCallback /></PublicRoute>} />
             
-            {/* Updated artist route */}
+            {/* Artist profile */}
             <Route path="artist/:code/:slug" element={<ArtistProfile />} />
             
+            {/* Public catalogues */}
             <Route path="catalogue/:catalogueId" element={<PublicCatalogue />} />
             <Route path="private-catalogue/:catalogueId" element={<PrivateCatalogue />} />
           </Route>
@@ -42,8 +45,16 @@ function App() {
           {/* Protected Routes */}
           <Route path="/dashboard" element={<ProtectedRoute><Layout /></ProtectedRoute>}>
             <Route index element={<Dashboard />} />
-            <Route path="artworks" element={<ArtworkManagement />} />
+            
+            {/* Artwork routes */}
+            <Route path="artworks" element={<ArtworkList />} />
+            <Route path="artworks/create" element={<ArtworkCreate />} />
+            <Route path="artworks/edit/:artworkId" element={<ArtworkEdit />} />
+
+            {/* Catalogue management */}
             <Route path="catalogues" element={<CatalogueManagement />} />
+            
+            {/* Settings */}
             <Route path="settings" element={<Settings />} />
           </Route>
         </Routes>
